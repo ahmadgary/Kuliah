34eb6b3b38a1b4b274ee28fb6585788803ae3faa
ahmadgary <59431221+ahmadgary@users.noreply.github.com>
2024-10-09 19:31:15 +0700

    Add files via upload

---
+++ Tugas Adythia/bilanganprima.py
@@ -0,0 +1,45 @@
+angka = int(input("masukan angka :"))
+prima = "adalah bilangan prima"
+
+
+if (angka == 1 or angka == 0):
+    prima = "bukan bilangan prima"
+for i in range (2, angka):
+    if (angka % i == 0):
+        prima = "bukan bilangan prima"
+print (angka, prima)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tugas Adythia/trapesium.py
@@ -0,0 +1,13 @@
+sisi_alas = float(input("Masukkan sisi alas trapesium (cm): "))
+sisi_atas = float(input("Masukkan sisi atas trapesium (cm): "))
+tinggi = float(input("Masukkan tinggi trapesium (cm): "))
+garis_miring1 = float(input("Masukkan garis miring pertama (cm): "))
+
+luas = 1/2*(sisi_alas + sisi_atas)*tinggi
+print("\nLuas Trapesium \t\t:",luas)
+
+keliling = sisi_alas + sisi_atas + garis_miring1 + garis_miring1
+print("\nKeliling Trapesium \t\t:",keliling)
+
+    
+     
